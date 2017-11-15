b1e62ace530b2064e91d92b8e31092c12248740a
qchenevier <qchenevier@users.noreply.github.com>
2017-11-15 07:08:15 +0100

    added standalone presentation generation

--- md2reveal.sh
+++ md2reveal.sh
@@ -2,12 +2,14 @@
 FILE=$1
 WINDOW_NAME=$2
 
-# generate_impress_html="pandoc --template impress-template.html -V impress-url=impress.js -s -t html4 --section-divs -o "$FILE".html "$FILE".md"
-generate_impress_html="pandoc -t revealjs -s -o "$FILE".html "$FILE".md"
+# generate_reveal_html="pandoc --template impress-template.html -V impress-url=impress.js -s -t html4 --section-divs -o "$FILE".html "$FILE".md"
+generate_reveal_html="pandoc -t revealjs -s -o "$FILE".html "$FILE".md"
+generate_reveal_standalone_html="pandoc -t revealjs -s --self-contained -o "$FILE"_standalone.html "$FILE".md"
 
 echo  $(date -Iseconds): generating html: $FILE
 
-if eval $generate_impress_html; then
+if eval $generate_reveal_html; then
+    eval $generate_reveal_standalone_html
     if [ "$(uname -s)" != "MSYS_NT-10.0" ]; then
         chrome_window_id=`xdotool search --name "("$WINDOW_NAME".*chrome)"`
         if [ -z "$chrome_window_id" ]; then
